b7db1f3e601cb19de1af18ce26bca57f1e20a7e9
kumavis <aaron@kumavis.me>
2017-01-18 17:01:32 -0800

    clean - editor - remove unused stuff

--- views/jsEditor.js
+++ views/jsEditor.js
@@ -1,7 +1,6 @@
 const vdom = require('virtual-dom')
 const h = require('virtual-hyperscript-hook')(vdom.h)
 const mount = require('virtual-hyperscript-mount')()
-const unmount = require('virtual-hyperscript-mount')()
 const CodeMirror = require('codemirror')
 require('codemirror/mode/javascript/javascript')
 const codemirrorStyle = require('fs').readFileSync(require.resolve('codemirror/lib/codemirror.css'), 'utf8')
@@ -11,21 +10,13 @@ module.exports = jsEditor
 function jsEditor() {
   var editor
   return (opts) => {
-
-    console.log('rerender!')
-    if (editor) {
-      console.log('updating editor...')
-      editor.setValue(opts.value)
-    } else {
-      console.log('no editor?')
-    }
+    if (editor) editor.setValue(opts.value)
     return h('div', [
       h('style', codemirrorStyle),
       h('div', {
         // auto focus
         hook: mount((el) => {
           el.value = opts.value
-          console.log('set editor...')
           editor = CodeMirror(el, {
             value: opts.value,
             mode:  'javascript',
@@ -34,10 +25,6 @@ function jsEditor() {
           })
           setTimeout(() => editor.refresh())
         }),
-        // unhook: unmount((el) => {
-        //   console.log('unhook!')
-        //   editor = null
-        // })
       }),
     ])
   }
